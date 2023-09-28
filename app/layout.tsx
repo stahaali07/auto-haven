503cb446377744ddf02e2726b65ae322cf0ca54e
stahaali07 <staha.ali07@gmail.com>
2023-09-28 13:09:22 +0500

    Hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'Auto Haven',
